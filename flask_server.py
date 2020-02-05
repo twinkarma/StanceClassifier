38d9fb06affd33de72086051c0ee2f615aac2502
Twin Karmakharm <hi@twin.uk.com>
2020-02-05 15:41:44 +0000

    Added conf file for for startup service

--- flask_server.py
+++ flask_server.py
@@ -13,22 +13,26 @@ def init_server():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    try:
 
-    if stance_classifier is None:
-        return jsonify({"error": "Could not start classifier"})
+        if stance_classifier is None:
+            return jsonify({"error": "Could not start classifier"})
 
-    if request.method == 'POST':
-        req_json = request.get_json()
-        if "original" in req_json.keys() and "reply" in req_json.keys():
-            print("Request received")
-            print(req_json["original"])
-            print(req_json["reply"])
+        if request.method == 'POST':
+            req_json = request.get_json()
+            if "original" in req_json.keys() and "reply" in req_json.keys():
+                print("Request received")
+                print(req_json["original"])
+                print(req_json["reply"])
 
-            stance_class, stance_prob = stance_classifier.classify(req_json["original"], req_json["reply"])
-            print("classified as {} {}".format(stance_class, stance_prob))
-            return jsonify({"stance_class": stance_class, "stance_prob": stance_prob.tolist()}), 200
+                stance_class, stance_prob = stance_classifier.classify(req_json["original"], req_json["reply"])
+                print("classified as {} {}".format(stance_class, stance_prob))
+                return jsonify({"stance_class": stance_class, "stance_prob": stance_prob.tolist()}), 200
 
-    return jsonify({"error": "Invalid input"})
+        return jsonify({"error": "Invalid input"}), 400
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 
