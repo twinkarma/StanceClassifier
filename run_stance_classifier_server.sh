fbf0b57b38b9f1d290e4ecbc50e5fdd87e1ab9e1
Twin Karmakharm <twin@gateservice10.dcs.shef.ac.uk>
2020-02-14 15:55:13 +0000

    Added a LANG env variable so that python in upstart knows to use UTF-8 encoding

--- run_stance_classifier_server.sh
+++ run_stance_classifier_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Script for starting the gunicorn host on the server
-
+export LANG=en_GB.UTF-8
 source /home/twin/miniconda3/bin/activate rumours
 exec gunicorn  --timeout 300 --bind 0.0.0.0:9125 wsgi
